da5fa8577e60a467899edac262db5a5d7b96faa0
natarajanrodrigues <natarajanrodrigues@gmail.com>
2017-05-04 02:56:19 -0300

    interviews pages for appraiser

--- rhecruta-web/src/main/java/br/edu/ifpb/dac/rhecruta/web/beans/AppraiserBean.java
+++ rhecruta-web/src/main/java/br/edu/ifpb/dac/rhecruta/web/beans/AppraiserBean.java
@@ -6,16 +6,15 @@
 package br.edu.ifpb.dac.rhecruta.web.beans;
 
 import br.edu.ifpb.dac.rhecruta.shared.domain.entities.Administrator;
+import br.edu.ifpb.dac.rhecruta.shared.domain.entities.Enterview;
 import br.edu.ifpb.dac.rhecruta.shared.domain.entities.User;
-import br.edu.ifpb.dac.rhecruta.shared.domain.vo.Credentials;
 import br.edu.ifpb.dac.rhecruta.shared.interfaces.AdministratorService;
+import br.edu.ifpb.dac.rhecruta.shared.interfaces.EnterviewService;
 import br.edu.ifpb.dac.rhecruta.shared.interfaces.UserService;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -37,18 +36,15 @@ public class AppraiserBean {
     @Inject
     private User loggedUser;
     
-    private Administrator administrator = new Administrator();
-    private User user = new User();
-    private Credentials credentials = new Credentials();
-    
-    private Administrator administratorUpdateble = new Administrator();
-
+    private Administrator loggedAppraiser;
         
+    @Inject
+    private EnterviewService interviewService;
+    
     @PostConstruct
     private void postConstruct() {
         
-        if (loggedUser != null) 
-            administratorUpdateble = administratorService.getByUser(getLoggedAdministrator().getUser());
+        this.loggedAppraiser = getLoggedAdministrator();
         System.out.println("Construi o AdministratorBean!");
     }
     
@@ -57,28 +53,6 @@ public class AppraiserBean {
         System.out.println("Destrui o AdministratorBean!");
     }
     
-    public String saveAdministrator() {
-        //
-        this.user.setCredentials(credentials);
-        this.administrator.setUser(user);
-        //
-        this.administratorService.save(administrator);
-        
-        System.out.println("[AdministratorBean: "+administrator+"]");
-        
-        return "result_request_register.xhtml?faces-redirect=true";
-    }
-    
-    public String updateAdministrator() {
-        
-        System.out.println("[aaaaaaaaaa Updatable: "+administratorUpdateble+"]");
-        
-        administratorService.update(administratorUpdateble);
-        
-        FacesContext.getCurrentInstance().addMessage("password-form", new FacesMessage("Successfull updated"));
-        
-        return null;
-    }
     
     public Administrator getLoggedAdministrator() {
         System.out.println("User: "+loggedUser);
@@ -89,45 +63,9 @@ public class AppraiserBean {
         } return null;
     }
     
-    public List<Administrator> getAdministratorsToApprove() {
-        return administratorService.listAdministratorsToApprove();
+    public List<Enterview> getInterviews(){
+        return interviewService.listByAppraiser(getLoggedAdministrator());
     }
     
-    public String respondRequest(Administrator administrator, boolean approve) {
-        administratorService.respondRequest(administrator, approve);
-        return null;
-    }
-
-    public Administrator getAdministrator() {
-        return administrator;
-    }
-
-    public void setAdministrator(Administrator administrator) {
-        this.administrator = administrator;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Credentials getCredentials() {
-        return credentials;
-    }
-
-    public void setCredentials(Credentials credentials) {
-        this.credentials = credentials;
-    }
-
-    public Administrator getAdministratorUpdateble() {
-        return this.administratorUpdateble;
-    }
-
-    public void setAdministratorUpdateble(Administrator administratorUpdateble) {
-        this.administratorUpdateble = administratorUpdateble;
-    }
     
 }
